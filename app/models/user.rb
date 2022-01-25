b4b06127b8bd25275a97853243a06202c492bb26
Gabriel S <thesouzagab@gmail.com>
2022-01-25 04:21:30 -0300

    feat: adds audits on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_paper_trail
+
   devise :database_authenticatable, :registerable, :recoverable, :validatable,
     :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
