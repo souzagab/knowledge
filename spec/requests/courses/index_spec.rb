05d0d1da479e7a1e99e24d53625c4a42c89a798a
Gabriel S <thesouzagab@gmail.com>
2022-01-25 20:36:54 -0300

    feat: adds authorization to users on courses
    users can see/read enrolled courses

--- spec/requests/courses/index_spec.rb
+++ spec/requests/courses/index_spec.rb
@@ -7,20 +7,6 @@ RSpec.describe "courses#index", type: :request do
 
       expect(response).to have_http_status :unauthorized
     end
-
-    context "authorization" do
-      it "responds with forbidden if user is not an admin" do
-        get url, headers: auth_headers
-
-        expect(response).to have_http_status :forbidden
-      end
-
-      it "requires user to be an admin" do
-        get url, headers: admin_headers
-
-        expect(response).to have_http_status :ok
-      end
-    end
   end
 
   context "when there are no existing courses" do
@@ -34,14 +20,52 @@ RSpec.describe "courses#index", type: :request do
   end
 
   context "when there are courses registered" do
-    let!(:existing_courses) { create_list :course, rand(1...3) }
+    let!(:existing_courses) { create_list :course, rand(3...5) }
 
-    it "responds with :ok status with courses's list" do
-      get url, headers: admin_headers
+    context "and the user is not a admin" do
+      let!(:user) { create :user }
+      let!(:headers) { auth_headers_for user }
 
-      expect(response).to have_http_status :ok
+      context "but the user does not have any enrollments" do
+        it "responds with :ok status and an empty list" do
+          get url, headers: headers
 
-      expect(response_body.size).to eq existing_courses.size
+          expect(response).to have_http_status :ok
+
+          expect(response_body).to eq []
+        end
+      end
+
+      context "and the user is registered in some courses" do
+        let(:enrolled_courses) { existing_courses.take rand(1...3) }
+
+        before do
+          enrolled_courses.each do |course|
+            create :enrollment, course: course, user: user
+          end
+        end
+
+        it "responds with :ok status with only the courses with enrollments" do
+          get url, headers: headers
+
+          expect(response).to have_http_status :ok
+
+          expect(response_body.size).to eq enrolled_courses.size
+
+          course_ids = response_body.pluck "id"
+          expect(course_ids).to match_array enrolled_courses.pluck(:id)
+        end
+      end
+    end
+
+    context "and the user is an admin" do
+      it "responds with :ok status with all courses" do
+        get url, headers: admin_headers
+
+        expect(response).to have_http_status :ok
+
+        expect(response_body.size).to eq existing_courses.size
+      end
     end
   end
 end
