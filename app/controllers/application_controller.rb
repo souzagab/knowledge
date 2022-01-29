52a2c9b75250f44766d34910566e54cca2dc71e5
Gabriel S <thesouzagab@gmail.com>
2022-01-29 17:52:04 +0000

    feat: adds catch-all route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found!
   rescue_from CanCan::AccessDenied, with: :forbidden!
 
-  private
+  def route_not_found!
+    render json: { error: "Route not found!" }, status: :not_found
+  end
 
   def not_found!(err = nil, message: "Resource not found!")
     render json: { error: err.try(:message) || message }, status: :not_found
