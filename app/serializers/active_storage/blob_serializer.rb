86c390076f5406a20fe41565e7c7717364aa212f
Gabriel S <thesouzagab@gmail.com>
2022-01-30 12:58:50 +0000

    feat: add upload headers to blobs serializer

--- app/serializers/active_storage/blob_serializer.rb
+++ app/serializers/active_storage/blob_serializer.rb
@@ -4,8 +4,7 @@ module ActiveStorage
     attributes :id, :filename, :content_type, :byte_size, :checksum
 
 
-    attribute(:signed_url) do
-      object.service_url_for_direct_upload
-    end
+    attribute(:signed_url)     { object.service_url_for_direct_upload }
+    attribute(:upload_headers) { object.service_headers_for_direct_upload }
   end
 end
